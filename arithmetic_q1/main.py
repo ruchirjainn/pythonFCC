2a1e76492035b4e2d6ad8598f8a7ceca3c671b8b
ruchirjainn <2021.ruchir.jain@ves.ac.in>
2023-06-26 00:14:51 +0530

    Your commit message

---
+++ arithmetic_q1/main.py
@@ -0,0 +1,7 @@
+from arit_formatter import arithmetic_arranger
+from unittest import main
+
+
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+
+main(module='test_module', exit=False)
